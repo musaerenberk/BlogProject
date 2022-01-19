dca0dc09ec8041dbfdcfa3a9fbd44c879dc50c4f
Musa Erenberk Karadeli <73529687+musaerenberk@users.noreply.github.com>
2022-01-19 11:43:16 +0300

    update

---
+++ Blog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string CreatedByName { get; set; }
+        public virtual string ModifiedByName { get; set; }
+
+
+    }
+}
