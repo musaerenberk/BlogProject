ae5bd24035c54bfb830af502224dfbeb9c20e291
Musa Erenberk Karadeli <73529687+musaerenberk@users.noreply.github.com>
2022-02-01 02:47:40 +0300

    started to create service layer

---
+++ Blog.Entities/Dtos/CategoryAddDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entities.Dtos
+{
+    public class CategoryAddDto
+    {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} boş geçilemez.")]
+        [MaxLength(70, ErrorMessage ="{0}{1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0}{1} karakterden az olmamalıdır.")]
+        public string Name { get; set; }
+
+
+
+    }
+}
---
+++ Blog.Services/Abstract/ICategoryService.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Entities.Concrete;
+using Blog.Entities.Dtos;
+using Blog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Services.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<IDataResult<Category>> Get(int categoryId);
+        Task<IDataResult<IList<Category>>> GetAll();
+        Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int categoryId);
+        Task<IResult> HardDelete(int categoryId);
+
+
+    }
+}
