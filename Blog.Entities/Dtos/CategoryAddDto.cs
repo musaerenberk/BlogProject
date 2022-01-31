16981fb082428ff32ad0fae3636755460fe24f3b
Musa Erenberk Karadeli <73529687+musaerenberk@users.noreply.github.com>
2022-02-01 02:58:06 +0300

    Category add and update dto class added

--- Blog.Entities/Dtos/CategoryAddDto.cs
+++ Blog.Entities/Dtos/CategoryAddDto.cs
@@ -15,7 +15,17 @@ namespace Blog.Entities.Dtos
         [MaxLength(70, ErrorMessage ="{0}{1} karakterden büyük olamaz.")]
         [MinLength(3, ErrorMessage = "{0}{1} karakterden az olmamalıdır.")]
         public string Name { get; set; }
-
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0}{1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0}{1} karakterden az olmamalıdır.")]
+        public string Description { get; set; }
+        [DisplayName("Kategori Özel Not Alanı")]
+        [MaxLength(500, ErrorMessage = "{0}{1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0}{1} karakterden az olmamalıdır.")]
+        public string Note { get; set; }
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} boş geçilemez.")]
+        public bool IsActive { get; set; }
 
 
     }
--- Blog.Entities/Dtos/CategoryUpdateDto.cs
+++ Blog.Entities/Dtos/CategoryUpdateDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,25 @@ namespace Blog.Entities.Dtos
 {
     public class CategoryUpdateDto
     {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} boş geçilemez.")]
+        [MaxLength(70, ErrorMessage = "{0}{1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0}{1} karakterden az olmamalıdır.")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0}{1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0}{1} karakterden az olmamalıdır.")]
+        public string Description { get; set; }
+        [DisplayName("Kategori Özel Not Alanı")]
+        [MaxLength(500, ErrorMessage = "{0}{1} karakterden büyük olamaz.")]
+        [MinLength(3, ErrorMessage = "{0}{1} karakterden az olmamalıdır.")]
+        public string Note { get; set; }
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        public bool IsActive { get; set; }
+        [DisplayName("Silindi Mi?")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        public bool IsDeleted { get; set; }
+
     }
 }
