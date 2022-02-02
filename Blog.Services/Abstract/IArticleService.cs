0301e853d10f2bfefb871a4ce569182a8ee193bf
Musa Erenberk Karadeli <73529687+musaerenberk@users.noreply.github.com>
2022-02-02 19:43:45 +0300

    Article Service created

---
+++ Blog.Services/Abstract/IArticleService.cs
@@ -0,0 +1,24 @@
+﻿using Blog.Entities.Concrete;
+using Blog.Entities.Dtos;
+using Blog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Services.Abstract
+{
+    public interface IArticleService
+    {
+        Task<IDataResult<ArticleDto>> Get(int articleId);
+        Task<IDataResult<ArticleListDto>> GetAll();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeleted();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActive();
+        Task<IDataResult<ArticleListDto>> GetAllByCategory(int categoryId);
+        Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName);
+        Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int articleId);
+        Task<IResult> HardDelete(int articleId);
+    }
+}
