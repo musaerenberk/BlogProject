ce145dd2faec33bcd0c69ace13232faf64808efb
Musa Erenberk Karadeli <73529687+musaerenberk@users.noreply.github.com>
2022-02-05 18:05:02 +0300

    Editing for Ajax post actions

--- Blog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ Blog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Blog.Mvc.Areas.Admin.Controllers
