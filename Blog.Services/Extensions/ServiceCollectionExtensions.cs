8892dcc6aa061397790c3cd6af403d02294d3bc1
Musa Erenberk Karadeli <73529687+musaerenberk@users.noreply.github.com>
2022-02-15 21:02:57 +0300

    user settings done

--- Blog.Services/Extensions/ServiceCollectionExtensions.cs
+++ Blog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Data.Abstract;
 using Blog.Data.Concrete;
 using Blog.Data.Concrete.EntityFramework.Contexts;
+using Blog.Entities.Concrete;
 using Blog.Services.Abstract;
 using Blog.Services.Concerete;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,7 +17,20 @@ namespace Blog.Services.Extensions
     {
         public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
         {
-            serviceCollection.AddDbContext<BlogContext>();
+            serviceCollection.AddDbContext<ProgrammersBlogContext>();
+            serviceCollection.AddIdentity<User, Role>(options =>
+            {
+                // User Password Options
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                // User Username and Email Options
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+$";
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<ProgrammersBlogContext>();
             serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
             serviceCollection.AddScoped<ICategoryService, CategoryManager>();
             serviceCollection.AddScoped<IArticleService, ArticleManager>();
