792df0d20646b49c7dab759140ad59fe143160b5
Musa Erenberk Karadeli <73529687+musaerenberk@users.noreply.github.com>
2022-02-15 20:51:40 +0300

    I completed the migration and database creation

--- Blog.Data/Migrations/20200926141946_InitialCreate.Designer.cs
+++ Blog.Data/Migrations/20200926141946_InitialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Blog.Data.Concrete.EntityFramework.Contexts;
+using ProgrammersBlog.Data.Concrete.EntityFramework.Contexts;
 
 namespace ProgrammersBlog.Data.Migrations
 {
     [DbContext(typeof(ProgrammersBlogContext))]
-    partial class ProgrammersBlogContextModelSnapshot : ModelSnapshot
+    [Migration("20200926141946_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
