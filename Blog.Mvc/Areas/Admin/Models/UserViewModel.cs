f5848ace18abf276eabdeed36a655639a9ef95cf
Musa Erenberk Karadeli <73529687+musaerenberk@users.noreply.github.com>
2022-02-17 22:08:19 +0300

    I Revised User Menu on TopBar as a ViewComponent

---
+++ Blog.Mvc/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Entities.Concrete;
+
+namespace Blog.Mvc.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+    }
+}
